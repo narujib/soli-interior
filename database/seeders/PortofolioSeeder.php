05f96c415da8f6488fa666fa339c7787dc14e6b7
najib <narujib13@gmail.com>
2025-03-23 08:12:23 +0700

    seeder

--- database/seeders/PortofolioSeeder.php
+++ database/seeders/PortofolioSeeder.php
@@ -5,6 +5,8 @@ namespace Database\Seeders;
 use App\Models\Portofolio;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Str;
 
 class PortofolioSeeder extends Seeder
 {
@@ -13,6 +15,24 @@ class PortofolioSeeder extends Seeder
      */
     public function run(): void
     {
-        Portofolio::factory(50)->create();
+        foreach (range(1, 50) as $index) {
+            $clientName = 'Client ' . $index;
+            $slug = Str::slug($clientName);
+
+            $count = DB::table('portfolios')->where('slug', 'LIKE', "$slug%")->count();
+            if ($count > 0) {
+                $slug = $slug . '-' . ($count + 1);
+            }
+
+            DB::table('portfolios')->insert([
+                'project_name' => 'Project ' . $index,
+                'description'  => 'This is the description for project ' . $index,
+                'client_name'  => $clientName,
+                'address'      => '123 Example St, City ' . $index . ', Country',
+                'slug'         => $slug,
+                'image'        => 'https://via.placeholder.com/640x480.png?text=Project+' . $index,
+                'date'         => now(),
+            ]);
+        }
     }
 }
